2044c29e24708a33188816f4d45ee19447269d32
Guru Theja <gurutejashetty@gmail.com>
2025-11-17 16:13:22 +0530

    Auto commit

--- variables/variable1.tf
+++ variables/variable1.tf
@@ -1,11 +1,27 @@
-variable "sample" {
+variable "samplestring" {
     default = "Hello World"
 }
 
 output "op" {
-    value = var.sample
+    value = var.samplestring
 }
 
 output "op1" {
-    value = "The value of sample variable is ${var.sample}"
+    value = "The value of sample variable is ${var.samplestring}"
+}
+
+variable "samplenumber" {
+    default = 20
+}
+
+output opnummber {
+    value = "The value number is ${var.samplenumber}"
+}
+
+variable "sampleboolean" {
+    default = true
+}
+
+output "opboolean" {
+    value = "The value number is ${var.sampleboolean}"
 }
