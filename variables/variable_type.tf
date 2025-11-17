9e311f59d6f97fced95e45dbc3e5de5144e977ba
Guru Theja <gurutejashetty@gmail.com>
2025-11-17 16:41:36 +0530

    Auto commit

---
+++ variables/variable_type.tf
@@ -0,0 +1,32 @@
+variable "sample" {
+    default = "Hello World"
+}
+
+output "opsample" {
+    value = var.sample
+}
+
+variable "list" {
+    default = [
+        "Hello",
+        100,
+        true
+        "World"
+    ]
+}
+
+output "oplist" {
+    value = var.list
+}
+
+variable "map" {
+    default = {
+        name = "Guru"
+        number = 100
+        boolean = true
+    }
+}
+
+output "opmap" {
+    value = var.map
+}
