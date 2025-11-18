59ea0018097684097099c8042f786dbb1956d641
Guru Theja <gurutejashetty@gmail.com>
2025-11-18 10:13:44 +0530

    Auto commit

--- vars/vars_practice.tf
+++ vars/vars_practice.tf
@@ -1,7 +1,23 @@
-variable "sample" {
+variable "string_sample" {
     default = "Hello World"
 }
 
-output "print_sample" {
-    value = "The value of sample variable is ${var.sample}"
+output "print_string_sample" {
+    value = "The value of sample variable is ${var.string_sample}"
+}
+
+variable "number_sample" {
+    default = 10
+}
+
+output "print_number_sample" {
+    value = "The value of number variable is ${var.number_sample}"
+}
+
+variable "boolean_sample" {
+    default = true
+}
+
+output "print_boolean_sample" {
+    value = "The value of number variable is ${var.boolean_sample}"
 }
