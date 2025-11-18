3aad5b68369539bf6af0044caaf8349cd8cba07e
Guru Theja <gurutejashetty@gmail.com>
2025-11-18 10:49:23 +0530

    Auto commit

--- vars/vars_practice.tf
+++ vars/vars_practice.tf
@@ -1,17 +1,19 @@
 variable "name" {}
-
 output "print_name" {
     value = "The value of the name variable is ${var.name}"
 }
 
 variable "topic" {}
-
 output "print_topic" {
     value = var.topic
 }
 
 variable "fee" {}
-
 output "print_fee" {
     value = var.fee
+}
+
+variable "trainer" {}
+output "print_trainer" {
+    value = var.trainer
 }
