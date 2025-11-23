7cacf90e9946403fb51caf79ca66d30377362a09
Guru Theja <gurutejashetty@gmail.com>
2025-11-23 13:43:50 +0530

    Auto commit

---
+++ 04-remote_state/main.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ami_id" {
+    most_recent = true
+    name_regex = "my_image_b55"
+    owners =  ["self"] #need to mention "self" otherwise it will check in the total internet
+}
+
+resource "aws_instance" "app" {
+    count = length(var.list)
+    ami = aws_ami.ami_id.image_id 
+    instance_type = t2.micro
+
+
+tags = {
+    Name = "${var.list[count.index]}"
+
+}
+}
+
+variable "list" {
+    default = ["cart", "dynamic", "DB"]
+}
